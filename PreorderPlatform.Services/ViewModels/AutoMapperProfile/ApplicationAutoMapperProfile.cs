3df5c084ec2adec60bf91171c41f2eaa15b1629f
Choids <hiep.bh.02@gmail.com>
2023-06-07 12:59:37 +0700

    automapper

--- PreorderPlatform.Services/ViewModels/AutoMapperProfile/ApplicationAutoMapperProfile.cs
+++ PreorderPlatform.Services/ViewModels/AutoMapperProfile/ApplicationAutoMapperProfile.cs
@@ -2,6 +2,7 @@
 using PreorderPlatform.Service.ViewModels.Business;
 using PreorderPlatform.Service.ViewModels.BusinessPaymentCredential;
 using PreorderPlatform.Service.ViewModels.Campaign;
+using PreorderPlatform.Service.ViewModels.CampaignDetail;
 using PreorderPlatform.Service.ViewModels.CampaignItem;
 using PreorderPlatform.Service.ViewModels.Category;
 using PreorderPlatform.Service.ViewModels.Order;
@@ -68,9 +69,9 @@ namespace PreorderPlatform.Service.ViewModels.AutoMapperProfile
                                 .ReverseMap();
 
             //CampaignDetail
-            CreateMap<PreorderPlatform.Entity.Entities.CampaignDetail, CampaignItemCreateViewModel>().ReverseMap();
-            CreateMap<PreorderPlatform.Entity.Entities.CampaignDetail, CampaiugnItemUpdateViewModel>().ReverseMap();
-            CreateMap<PreorderPlatform.Entity.Entities.CampaignDetail, CampaignItemViewModel>().ReverseMap();
+            CreateMap<PreorderPlatform.Entity.Entities.CampaignDetail, CampaignDetailCreateViewModel>().ReverseMap();
+            CreateMap<PreorderPlatform.Entity.Entities.CampaignDetail, CampaignDetailUpdateViewModel>().ReverseMap();
+            CreateMap<PreorderPlatform.Entity.Entities.CampaignDetail, CampaignDetailViewModel>().ReverseMap();
 
             //Order
             CreateMap<PreorderPlatform.Entity.Entities.Order, OrderCreateViewModel>().ReverseMap();
